356d35909018b038ccff4cb825ec844504b229cf
Stefan <s1225502@student.windesheim.nl>
2026-03-08 19:03:25 +0100

    Added typing to class

--- DP6/recept.py
+++ DP6/recept.py
@@ -1,27 +1,37 @@
+from stap import Stap
+from ingredient import Ingredient
+
+
 class Recept:
-    def __init__(self, naam, omschrijving, aantalPersonen = 1):
+    def __init__(self,
+                 naam:str,
+                 omschrijving: str,
+                 aantalPersonen = 1):
         self.__naam = naam
         self.__omschrijving = omschrijving
         self.__ingredient_list = []
-        self.__stappen = []
+        self.__stappen: list[Stap] = []
         self.__aantalPersonen = aantalPersonen
 
-    def voeg_ingredient_toe(self, ingredient):
-        self.__ingredient_list.append(ingredient)
-
-    def get_ingredienten(self):
-        return self.__ingredient_list
+    def get_naam(self):
+        return self.__naam
     
+    def set_naam(self, naam):
+        self.__naam = naam
+
     def voeg_omschrijving_toe(self, omschrijving):
         self.__omschrijving = omschrijving
 
     def get_omschrijving(self):
         return self.__omschrijving
+
+    def voeg_ingredient_toe(self, ingredient: Ingredient):
+        self.__ingredient_list.append(ingredient)
+
+    def get_ingredienten(self):
+        return self.__ingredient_list
     
-    def get_naam(self):
-        return self.__naam
-    
-    def voeg_stap_toe(self, stap):
+    def voeg_stap_toe(self, stap: Stap):
         self.__stappen.append(stap)
 
     def get_stappen(self):
@@ -34,7 +44,7 @@ class Recept:
         return self.__aantalPersonen
 
     def get_plantaardig_recept(self, plantaardig):
-        print("Plantaardig")
+        return True
 
     def __str__(self):
         return f"{self.__naam} geschreven door Stefan Brinkman \n Studentnummer: 1225502 Klas: ICTBC2j"
