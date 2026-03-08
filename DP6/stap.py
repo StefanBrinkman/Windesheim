e20d1b2667329d2c614c650ad998d87156256e5e
Stefan <s1225502@student.windesheim.nl>
2026-03-08 18:33:49 +0100

    Updated Stap class with getters and setters

--- DP6/stap.py
+++ DP6/stap.py
@@ -1,10 +1,19 @@
 class Stap:
-    def __init__(self, beschrijving, tip=None):
+    def __init__(self, beschrijving:str, tip=None):
         self.__beschrijving = beschrijving
         self.__tip = tip
     
     def get_beschrijving(self):
         return self.__beschrijving
     
+    def set_beschrijving(self, beschrijving):
+        self.__beschrijving = beschrijving
+
+    def set_tip(self, tip):
+        self.__tip = tip
+
+    def get_tip(self):
+        return self.__tip
+
     def __str__(self):
         return f"Informatie over stap met als beschrijving {self.__beschrijving}"
